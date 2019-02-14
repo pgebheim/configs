8e98be7afea92cd0038129d884e9ed9541397a47
Paul Gebheim <paul@onetaste.us>
2019-02-14 23:14:49 +0000

    Add zsh to config

--- setup_config.sh
+++ setup_config.sh
@@ -3,3 +3,7 @@ ln -s $1/vim/.vim .vim
 ln -s $1/vim/.vimrc .vimrc
 ln -s $1/git/.gitconfig .gitconfig
 ln -s $1/git/.gitshrc .gitshrc
+ln -s $1/.zshrc .zshrc
+ln -s $1/.oh-my-sh zsh
+
+git clone https://github.com/robbyrussell/oh-my-zsh.git .oh-my-zsh
